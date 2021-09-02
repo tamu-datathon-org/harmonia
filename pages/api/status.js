74eaf0360ae06b44639216b84299d472e5ced28b
jacksebastian17 <jacksebastian17@tamu.edu>
2021-09-01 23:24:02 -0500

    api endpoint to return all discord users username and discriminator (for dan)

--- pages/api/status.js
+++ pages/api/status.js
@@ -12,7 +12,7 @@ const db = mongoose.connect(process.env.MONGODB_URI,
 const UserSchema = new mongoose.Schema({
   discordId: { type: String, required: true },
   username: { type: String, required: true },
-  discriminator: {type: String, required: true },
+  discriminator: {type: String, required: true }, 
   authId: {type: String, required: true }
 });
 
@@ -39,18 +39,24 @@ handler.get(authenticatedRoute(async (req, res, tdUser) => {
     await client.login(process.env.DISCORDBOT_TOKEN); // harmonia token
     const guild = new Discord.Guild(client, guildId);
     try {
-        const guildFetchPromise = guild.fetch();
-        const user = await DiscordUser.findOne({ authId: tdUser.authId });
-        await guildFetchPromise;
-        const discUser = await guild.members.fetch(user.discordId);
-        const isMember = discUser.roles.cache.has(process.env.ROLE_ID)
-        if (discUser) {
-            return res.status(200).json({isInServer : true, isMember})
-        }
+      //console.log("here1");
+      const guildFetchPromise = guild.fetch();
+      const user = await DiscordUser.findOne({ authId: tdUser.authId });
+      //console.log("here2");
+      await guildFetchPromise;
+      //console.log("here3");
+      const discUser = await guild.members.fetch(user.discordId);
+      //console.log("here4");
+      const isMember = discUser.roles.cache.has(process.env.ROLE_ID)
+      //console.log("here5");
+      if (discUser) {
+        //console.log("here6");
+        return res.status(200).json({isInServer : true, isMember})
+      }
     }
     catch(err) {
-        console.log(err);
-        res.status(200).json({isInServer : false, isMember : false})
+      console.log(err);
+      res.status(200).json({isInServer : false, isMember : false})
     }
   }
   catch(err) {
---
+++ pages/api/users.js
@@ -0,0 +1,41 @@
+import nextConnect from 'next-connect'
+import passport from 'passport'
+import session from 'express-session';
+import { authenticatedRoute } from '../../libs/middleware'
+const DiscordStrategy = require('passport-discord').Strategy;
+const mongoose = require('mongoose');
+const Discord = require('discord.js');
+
+const db = mongoose.connect(process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true})
+
+const UserSchema = new mongoose.Schema({
+  discordId: { type: String, required: true },
+  username: { type: String, required: true },
+  discriminator: {type: String, required: true },
+  authId: {type: String, required: true }
+});
+
+const DiscordUser = mongoose.models.User || mongoose.model('User', UserSchema);
+
+const handler = nextConnect();
+
+handler.get(authenticatedRoute(async (req, res) => {
+  try {
+    const users = await DiscordUser.find();
+    const numUsers = await DiscordUser.countDocuments().then();
+    var allUsers = [];
+    var i = 0;
+    while (i < numUsers) {
+      allUsers.push(users[i].username + "#" + users[i].discriminator);
+      i += 1;
+    }
+    res.json({discordUsers : allUsers});
+  }
+  catch(err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+}));
+
+export default handler;
